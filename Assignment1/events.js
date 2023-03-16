e16a51fa461cc5a5f719a915ff60b9c44e240157
Dantheyman <109797370+Dantheyman@users.noreply.github.com>
2023-03-16 14:47:07 +1300

    Add files via upload

--- Assignment1/events.js
+++ Assignment1/events.js
@@ -3,12 +3,12 @@ var request = new XMLHttpRequest();
 
 window.addEventListener('load',function(){
 
-    getDataAjax("events.php?event=0");   
+    getDataAjax("AJAXevents.php?event=0");   
     
 });
 function getDataAjax(url)
 {
-    
+  
     request.addEventListener("load", displayData);
     request.open("GET", url);
 	request.send("");
@@ -20,9 +20,46 @@ function displayData()
     var response = request.responseText;
     var div = document.getElementById("data");
     div.innerHTML= response;
+    
+    lat=getLat();
+    lon=getLon();
+    getWeather(lat,lon);
 }
 
+async function getWeather(lat,lon)
+{
 
+    if(lat)
+    {
+        let response= await fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=749ff8538d7dcd9b857624af877fb0ec");
+        let data = await response.json();
+        console.log("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=749ff8538d7dcd9b857624af877fb0ec");
+        console.log(data);
+    }
+    
+}
+
+function getLat()
+{
+    if (div = document.getElementById("lat"))
+    {
+        return div.textContent;
+    }
+   
+    
+
+}
+
+function getLon()
+{
+    
+    if (div = document.getElementById("lon"))
+    {
+        return div.textContent.trim();
+    }
+    
+
+}
 
 
 	
